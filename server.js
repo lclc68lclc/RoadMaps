8719702bf15c95a8c7038c5984092743e1cbef26
Lina Clark <willina.clark@gmail.com>
2019-02-06 12:31:39 -0500

    testing again

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ let Roadmap = require('./roadmap.model');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/build')));
-
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/roadmaps", { useNewUrlParser: true });
