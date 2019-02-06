736fb3621e515022dddbe4ffe1c6ae2481ba37c4
Lina Clark <willina.clark@gmail.com>
2019-02-06 13:15:25 -0500

    updating the port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const roadmapRoutes = express.Router();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 let Roadmap = require('./roadmap.model');
 app.use(cors());
